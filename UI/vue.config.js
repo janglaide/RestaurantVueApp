645813ea8431e6a33ca51b3636929a76b0e42a3a
Alisa Proshchenko <ajanglaide@gmail.com>
2023-05-01 11:44:11 +0300

    ui project configs cleanup

--- UI/vue.config.js
+++ UI/vue.config.js
@@ -1,31 +1,32 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
 const baseFolder =
-    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-        ? `/${process.env.APPDATA}/ASP.NET/https`
-        : `/${process.env.HOME}/.aspnet/https`;
+  process.env.APPDATA !== undefined && process.env.APPDATA !== ""
+    ? `${process.env.APPDATA}/ASP.NET/https`
+    : `${process.env.HOME}/.aspnet/https`;
 
-const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
+const certificateArg = process.argv
+  .map((arg) => arg.match(/--name=(?<value>.+)/i))
+  .filter(Boolean)[0];
 const certificateName = certificateArg ? certificateArg.groups.value : "UI";
 
-console.log(certificateName)
+console.log(certificateName);
 if (!certificateName) {
-    console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
-    process.exit(-1);
+  console.error(
+    "Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly."
+  );
+  process.exit(-1);
 }
 
-//const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const certFilePath = "C:/Users/alisa.proshchenko/AppData/Roaming/ASP.NET/https/ui.pem";
-//const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-const keyFilePath = "C:/Users/alisa.proshchenko/AppData/Roaming/ASP.NET/https/ui.key";
-
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 module.exports = {
-    devServer: {
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        },
-        port: 5002
-    }
-}
+  devServer: {
+    https: {
+      key: fs.readFileSync(keyFilePath),
+      cert: fs.readFileSync(certFilePath),
+    },
+    port: 5002,
+  },
+};
